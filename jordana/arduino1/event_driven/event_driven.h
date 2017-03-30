144bd859410d2fa2c5fbd6a8ccb322dd603e7f6d
jordanamecler <jordana.mecler@hotmail.com>
2017-03-29 22:59:03 -0300

    consertando tarefa 2

--- jordana/arduino1/event_driven/event_driven.h
+++ jordana/arduino1/event_driven/event_driven.h
@@ -1,2 +1,2 @@
-void button_listen (int pin);
-void timer_set (int ms);
+void button_listen(int pin);
+void timer_set(int ms);
--- jordana/arduino1/event_driven/event_driven.ino
+++ jordana/arduino1/event_driven/event_driven.ino
@@ -1,43 +1,42 @@
 #include "event_driven.h"
 #include "arduino1.h"
 
-int old = millis ();
-int valorAntigo[3];
+unsigned long old = millis();
 unsigned long timer = 0;
+int valorAntigo[3];
 int observaBotao[3];
 
-void setup () {
-  Serial.begin(9600);
+void setup() {
   int i;
-  for (i = 0; i < 3; i++) {
+  for(i = 0; i < 3; i++) {
     observaBotao[i] = 0;
     valorAntigo[i] = 0;
    }
-  inicia ();
+  inicia();
 }
 
-void button_listen (int pin) {
-  pinMode (pin, INPUT);
+void button_listen(int pin) {
+  pinMode(pin, INPUT);
   observaBotao[pin - KEY1] = 1;
 }
 
-void timer_set (int ms) {
+void timer_set(int ms) {
   timer = ms;
 }
 
-void loop () {
+void loop() {
   
-  if (millis () - old >= timer) {
+  if(millis() - old >= timer) {
+    timer_expired();  
     old = millis();
-    timer_expired ();  
   }
   
   int i;
-  for (i = 0; i < 3; i++) {
-    if (observaBotao[i]) {
-      int valor = digitalRead (i + KEY1);
-      if (valor != valorAntigo[i]) {
-        button_changed (i + KEY1, valor);
+  for(i = 0; i < 3; i++) {
+    if(observaBotao[i]) {
+      int valor = digitalRead(i + KEY1);
+      if(valor != valorAntigo[i]) {
+        button_changed(i + KEY1, valor);
         valorAntigo[i] = valor;
       }
     }
