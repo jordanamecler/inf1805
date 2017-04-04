149cc898857024e7bd01f64b9e9833e45480b91a
Jordana Mecler <jordana.mecler@hotmail.com>
2017-04-04 18:58:14 -0300

    Add files via upload

---
+++ jordana/lua1/main.lua
@@ -0,0 +1,43 @@
+-- renomear para main.lua
+
+function retangulo (x,y,w,h)
+  local originalx, originaly, rx, ry, rw, rh = x, y, x, y, w, h
+  return {
+     draw =
+	function ()
+	   love.graphics.rectangle("line", rx, ry, rw, rh)
+	end,
+     keypressed =
+	function (key)
+	   local mx, my = love.mouse.getPosition()
+	   if key == 'b' and naimagem (mx,my, x, y) then
+     		y = 200
+     		x = 50
+  	   elseif key == "down" and naimagem(mx, my, x, y)  then
+     		y = y + 10
+  	   elseif key == "right" and naimagem(mx, my, x, y) then
+     		x = x + 10
+  	   end
+	end
+  }
+end
+      
+function love.load()
+  ret1 = retangulo (50, 200, 200, 150)
+end
+
+function naimagem (mx, my, x, y) 
+  return (mx>x) and (mx<x+w) and (my>y) and (my<y+h)
+end
+
+function love.keypressed(key)
+  ret1.keypressed(key)
+end
+
+function love.update (dt)
+  local mx, my = love.mouse.getPosition() 
+end
+
+function love.draw ()
+  ret1.draw()
+end
