34757fa102c996920ebc12228e65167e88ede129
Jordana Mecler <jordana.mecler@hotmail.com>
2017-04-11 15:41:14 -0300

    Add files via upload
    funcionando encapsulado com 2 retangulos

--- jordana/lua1/main.lua
+++ jordana/lua1/main.lua
@@ -10,13 +10,13 @@ function retangulo (x,y,w,h)
      keypressed =
 	function (key)
 	   local mx, my = love.mouse.getPosition()
-	   if key == 'b' and naimagem (mx,my, x, y) then
-     		y = 200
-     		x = 50
-  	   elseif key == "down" and naimagem(mx, my, x, y, w, h)  then
-     		y = y + 10
-  	   elseif key == "right" and naimagem(mx, my, x, y, w, h) then
-     		x = x + 10
+	   if key == 'b' and naimagem (mx,my, x, y, rw, rh) then
+     		ry = originalx
+     		rx = originaly
+  	   elseif key == "down" and naimagem(mx, my, x, y, rw, rh)  then
+     		ry = ry + 10
+  	   elseif key == "right" and naimagem(mx, my, x, y, rw, rh) then
+     		rx = rx + 10
   	   end
 	end,
      update =
@@ -28,20 +28,24 @@ end
       
 function love.load()
   ret1 = retangulo (50, 200, 200, 150)
+  ret2 = retangulo (300, 200, 200, 150)
 end
 
-function naimagem (mx, my, x, y, w, h) 
-  return (mx>x) and (mx<x+w) and (my>y) and (my<y+h)
+function naimagem (mx, my, x, y, rw, rh) 
+  return (mx>x) and (mx<x+rw) and (my>y) and (my<y+rh)
 end
 
 function love.keypressed(key)
   ret1.keypressed(key)
+  ret2.keypressed(key)
 end
 
 function love.update ()
   ret1.update()
+  ret2.update()
 end
 
 function love.draw ()
   ret1.draw()
+  ret2.draw()
 end
