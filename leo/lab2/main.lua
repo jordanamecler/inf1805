22d258791b74811f84c643f6f109db81bd85985a
Leonardo Edelman Wajnsztok <leo.wajnsztok@gmail.com>
2017-04-12 14:35:21 -0300

    Update main.lua

--- leo/lab2/main.lua
+++ leo/lab2/main.lua
@@ -1,32 +1,33 @@
 -- renomear para main.lua
 
 function love.load()
-  x = 50 y = 200
-  w = 200 h = 150
 
-  ret1 = retangulo(50, 100, 200, 150);
-  ret2 = retangulo(50, 300, 200, 150);
+  vect = {retangulo(50, 100, 200, 150), retangulo(50, 300, 200, 150), retangulo(10, 10, 300, 300)};
+
 end
 
-function naimagem (mx, my, x, y)
-  return (mx>x) and (mx<x+w) and (my>y) and (my<y+h)
+function naimagem (mx, my, x, y, rw, rh)
+	print(mx, my, x, y, rw, rh)
+  return (mx>x) and (mx<x+rw) and (my>y) and (my<y+rh)
 end
 
 function love.keypressed(key)
-  local mx, my = love.mouse.getPosition()
-  ret1.keypressed(key)
-  ret2.keypressed(key)
+  for i = 1, #vect do
+	vect[i].keypressed(key)
+  end
 
 end
 
 function love.update (dt)
-	ret1.update()
-	ret2.update()
+	for i = 1, #vect do
+		vect[i].update()
+	end
 end
 
 function love.draw ()
-  ret1.draw()
-  ret2.draw()
+  for i = 1, #vect do
+	vect[i].draw()
+  end
 end
 
 function retangulo (x, y, w, h)
@@ -40,18 +41,20 @@ function retangulo (x, y, w, h)
 
 		keypressed =
 		function(key)
-			if key == 'b' and naimagem (mx,my, x, y) then
-				ry = 200
-			elseif key == "down" and naimagem(mx, my, rx, ry)  then
+			mx, my = love.mouse.getPosition()
+			if key == 'b' and naimagem (mx,my, x, y, rw, rh) then
+				ry = originaly
+				rx = originalx
+			elseif key == "down" and naimagem(mx, my, rx, ry, rw, rh)  then
 				ry = ry + 10
-			elseif key == "right" and naimagem(mx,my,rx,ry) then
+			elseif key == "right" and naimagem(mx,my,rx,ry, rw, rh) then
 				rx = rx + 10
 			end
 		end,
 
 		update =
 		function()
-			local mx, my = love.mouse.getPosition()
+			mx, my = love.mouse.getPosition()
 		end
 	}
 end
