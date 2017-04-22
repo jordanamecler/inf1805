47258fe7db32f25a550a8e255b82a959aada85c1
jordanamecler <jordana.mecler@hotmail.com>
2017-04-21 21:48:29 -0300

    cobra come os quadrados e fica maior

--- projeto2/main.lua
+++ projeto2/main.lua
@@ -1,13 +1,11 @@
-function newblip (vel)
+function newblip ()
   local width, height = love.graphics.getDimensions( )
-  local x, y = love.math.random(width), love.math.random(height)
+  local x, y = love.math.random(width - 10), love.math.random(height - 10)
   return {
-    timeInactive = 0,
-    update = function (self)
+    update = function (dt)
 		end,
-    affected = function (pos)
-      if pos>x and pos<x+10 then
-      -- "pegou" o blip
+    affected = function (playerWidth, playerHeight)
+      if playerWidth>x and playerWidth<x+10 and playerHeight>y and playerHeight<y+10 then
         return true
       else
         return false
@@ -22,42 +20,44 @@ end
 function newplayer ()
   local lastPos = "right"
   local x, y = 0, 200
+  local tail = 0
   local playerWidth = x + 30
   local playerHeight = y
   local width, height = love.graphics.getDimensions( )
   return {
-  try = function ()
-    return x
-  end,
   update = function (dt)
-    if lastPos == "right" then
+    local hit = bls.affected(playerWidth, playerHeight)
+    if hit then
+      tail = tail + 10
+      bls = newblip()
+    elseif lastPos == "right" then
       x = x + 0.5
-      if x > width then
-        x = 0
-      end
-      playerWidth = x + 30
+      playerWidth = x + 30 + tail
       playerHeight = y
+      if playerWidth > width then
+        lastPos = "lost"
+      end
     elseif lastPos == "left" then
       x = x - 0.5
-      if x < 0 then
-        x = width
-      end
-      playerWidth = x + 30
+      playerWidth = x - 30 - tail
       playerHeight = y
+      if playerWidth < 0 then
+        lastPos = "lost"
+      end
     elseif lastPos == "up" then
       y = y - 0.5
-      if y > height then
-        y = 0
-      end
       playerWidth = x
-      playerHeight = y + 30
+      playerHeight = y - 30 - tail
+      if playerHeight < 0 then
+        lastPos = "lost"
+      end
     elseif lastPos == "down" then
       y = y + 0.5
-      if y < 0 then
-        y = height
-      end
       playerWidth = x
-      playerHeight = y + 30
+      playerHeight = y + 30 + tail
+      if playerHeight > height then
+        lastPos = "lost"
+      end
     end
   end,
   draw = function ()
@@ -67,25 +67,25 @@ function newplayer ()
     if key == "down" then
       if lastPos == "down" or lastPos == "up" then
         return
-      else
+      elseif lastPos == "right" or lastPos == "left" then
         lastPos = "down"
       end
     elseif key == "up" then
       if lastPos == "down" or lastPos == "up" then
         return
-      else
+      elseif lastPos == "right" or lastPos == "left" then
         lastPos = "up"
       end
     elseif key == "right" then
       if lastPos == "right" or lastPos == "left" then
         return
-      else
+      elseif lastPos == "down" or lastPos == "up" then
         lastPos = "right"
       end
     elseif key == "left" then
       if lastPos == "right" or lastPos == "left" then
         return
-      else
+      elseif lastPos == "down" or lastPos == "up" then
         lastPos = "left"
       end
     end
@@ -95,41 +95,19 @@ end
 
 function love.keypressed(key)
   player.keypressed (key)
-  if key == 'a' then
-    pos = player.try()
-    for i in ipairs(listabls) do
-      local hit = listabls[i].affected(pos)
-      if hit then
-        table.remove(listabls, i) -- esse blip "morre"
-        listabls[1] = newblip(1)
-        return -- assumo que só vai pegar um blip
-      end
-    end
-  end
 end
 
 function love.load()
-  tempo = 0
   player =  newplayer()
-  listabls = {}
-  listabls[1] = newblip(1)
+  bls = newblip()
 end
 
 function love.draw()
   player.draw()
-  for i = 1,#listabls do
-    listabls[i].draw()
-  end
+  bls.draw()
 end
 
 function love.update(dt)
-  tempo = tempo + dt
   player.update(dt)
-  for i = 1,#listabls do
-    if listabls[i].timeInactive == 0 then
-       listabls[i]:update()
-    elseif tempo > listabls[i].timeInactive then
-       listabls[i].timeInactive = 0
-    end
-  end
+  bls.update(dt)
 end
