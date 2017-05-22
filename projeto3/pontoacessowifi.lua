f767a62be31431a7f6a33c168180e4b4e1e6e218
jordanamecler <jordana.mecler@hotmail.com>
2017-05-22 20:43:01 -0300

    inicio transicao telas

--- projeto3/pontoacessowifi.lua
+++ projeto3/pontoacessowifi.lua
@@ -1,5 +1,5 @@
 -- Conexao na rede Wifi
 wifi.setmode(wifi.SOFTAP)
-wifi.ap.config({ssid="j-l_nodemcu",pwd="admin"})
+wifi.ap.config({ssid="j-l_nodemcu",pwd="kahoot10"})
 wifi.ap.setip({ip="192.168.0.66",netmask="255.255.255.0",gateway="192.168.0.20"})
 print(wifi.ap.getip())
--- projeto3/servidorbasico.lua
+++ projeto3/servidorbasico.lua
@@ -1,45 +1,85 @@
-stopLed = 3
-startLed = 6
+led1 = 3
+led2 = 6
 
-startButton = 1
-stopButton = 2
+local led={}
+led[0]="OFF"
+led[1]="OFF"
 
-gpio.mode(startLed, gpio.OUTPUT)
-gpio.mode(stopLed, gpio.OUTPUT)
-gpio.mode(startButton, gpio.INT, gpio.PULLUP)
-gpio.mode(stopButton, gpio.INT, gpio.PULLUP)
+local questions={}
+local answers={}
 
-gpio.write(startLed, gpio.LOW);
-gpio.write(stopLed, gpio.LOW);
+local function createTest()
+  buf = [[
+              <div style="width: 40%; margin: 0 auto;">
+                <h1><u>Kahoot but better</u></h1>
+                <h3>Write your questions! </h3>
 
+                <p>Test's title: <input sytle="width:300px" type="text" name="title"/></p>
+                
+                <form method="POST" action="STARTTEST">
+                  <p>1) Pergunta:<input style="width:600px" type="text" name="question1"/></p>
+                  <p>a) <input style="width:600px" type="text" name="answer11"/></p>
+                  <p>b) <input style="width:600px" type="text" name="answer12"/></p>
+                  <p>c) <input style="width:600px" type="text" name="answer13"/></p>
 
--- temp = tmr.create()
--- temp:register(speed, tmr.ALARM_AUTO,
--- 			function (t)
--- 			  
--- 			end)
--- temp:start()
+                  <p>2) Pergunta: <input style="width:600px" type="text" name="question2"/></p>
+                  <p>a) <input style="width:600px" type="text" name="answer21"/></p>
+                  <p>b) <input style="width:600px" type="text" name="answer22"/></p>
+                  <p>c) <input style="width:600px" type="text" name="answer23"/></p>
 
-gpio.trig(button1, "down", function(level)
+                  <p>3) Pergunta: <input style="width:600px" type="text" name="question3"/></p>
+                  <p>a) <input style="width:600px" type="text" name="answer31"/></p>
+                  <p>b) <input style="width:600px" type="text" name="answer32"/></p>
+                  <p>c) <input style="width:600px" type="text" name="answer33"/></p>
 
-end)
-			 
-gpio.trig(button2, "down", function(level)
+                  <input type="submit" value="Start test! uhul"/>
+                </form>
 
-end)
-			 
-local status={}
-sw[1]="Comecou"
-sw[0]="Terminou"
+                <p>
+            </div>
+            ]]
+end
 
-function home_view()
+local function startTest()
+  buf = [[
+          <div style="width: 40%; margin: 0 auto;">
+            <h1><u>Kahoot but better</u></h1>
+            <h3>On ur marks, ready, steady, 1, 2, 3, jacareh, jabuti, go!!1! </h3>
 
-local urls = {
-  home = nil,
-  start = nil,
-  finished = nil,
-  stop = nil,
-  score = nil,
+            <h4>Test title</h4>
+            
+            <form method="POST" action="finished">
+              <p>1) Pergunta: $QUESTION1</p>
+
+              <input type="radio" name="answer1" value="$ANSWER11"> Resposta A 
+              <input type="radio" name="answer1" value="$ANSWER12"> Resposta B
+              <input type="radio" name="answer1" value="$ANSWER13"> Resposta C
+
+              <p>2) Pergunta: $QUESTION2</p>
+
+              <input type="radio" name="answer2" value="$ANSWER21"> Resposta A
+              <input type="radio" name="answer2" value="$ANSWER22"> Resposta B
+              <input type="radio" name="answer2" value="$ANSWER23"> Resposta C
+
+              <p>3) Pergunta: $QUESTION3</p>
+              
+              <input type="radio" name="answer3" value="$ANSWER31"> Resposta A
+              <input type="radio" name="answer3" value="$ANSWER32"> Resposta B
+              <input type="radio" name="answer3" value="$ANSWER33"> Resposta C
+              <br>
+              <br>
+              <input type="submit" value="I WANT SOME ANSWERS ~rage quit~"/>
+            </form>
+
+            <p>
+          </div>
+
+        ]]
+end
+
+local actions = {
+  CREATETEST = createTest,
+  STARTTEST = startTest,
 }
 
 srv = net.createServer(net.TCP)
@@ -48,7 +88,7 @@ function receiver(sck, request)
 
   -- analisa pedido para encontrar valores enviados
   local _, _, method, path, vars = string.find(request, "([A-Z]+) ([^?]+)%?([^ ]+) HTTP");
-  -- se nÃ£o conseguiu casar, tenta sem variÃ¡veis
+  -- se nÃ£o conseguiu casar, tenta sem variaveis
   if(method == nil)then
     _, _, method, path = string.find(request, "([A-Z]+) (.+) HTTP");
   end
@@ -56,35 +96,21 @@ function receiver(sck, request)
   local _GET = {}
   
   if (vars ~= nil)then
+    print(vars)
     for k, v in string.gmatch(vars, "(%w+)=(%w+)&*") do
       _GET[k] = v
     end
   end
 
+  buf = [[
+  <a href="?pin=CREATETEST"><button><b>Create Test</b></button></a>
+  ]]
 
-  local action = urls[_GET.action]
+  local action = actions[_GET.pin]
   if action then action() end
 
   local vals = {
-    CHV1 = gpio.LOW,
-    CHV2 = gpio.LOW,
-    LED1 = led[gpio.read(led1)],
-    LED2 = led[gpio.read(led2)],
   }
-
-  local buf = [[
-<h1><u>Kahoot soh que melhor</u></h1>
-<p><a href="?pin=LERTEMP"><button><b>REFRESH</b></button></a>
-<p>
-LED 1: $LED1  :  
-	<a href="?pin=LIGA1"><button><b>ON</b></button></a>
-	<a href="?pin=DESLIGA1"><button><b>OFF</b></button></a>
-</p>
-<p>LED 2: $LED2  :  
-	<a href="?pin=LIGA2"><button><b>ON</b></button></a>
-	<a href="?pin=DESLIGA2"><button><b>OFF</b></button></a>
-</p>
-]]
 
   buf = string.gsub(buf, "$(%w+)", vals)
   sck:send(buf, function() print("respondeu") sck:close() end)
