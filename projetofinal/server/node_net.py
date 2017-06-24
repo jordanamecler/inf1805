6aeca4bbd38093508fc275e2ff59ec2d4147a706
leotok <leo.wajnsztok@gmail.com>
2017-06-24 01:51:01 -0300

    some topics and readme

---
+++ projetofinal/server/node_net.py
@@ -0,0 +1,35 @@
+class Node(object):
+
+	def __init__(self, number, name=""):
+
+		self.number = number
+		if name:
+			self.name = name
+		else:
+			self.name = "node" + str(number)
+		self.neighbours = dict()
+
+	def __str__(self):
+		return "Node " + str(self.number) + ": " + self.name
+
+	def add_neighbour(self, node):
+		self.neighbours[node.number] = node
+
+class NodeNet(object):
+
+	def __init__(self):
+		self.nodes = dict()
+
+	def add_node(self, number, name):
+		node = Node(number, name)
+		self.nodes[number] = node
+		return node
+
+	def get_node(self, number):
+		try:
+			node = self.nodes[number]
+			return node
+		except:
+			return None
+
+
--- projetofinal/server/server.py
+++ projetofinal/server/server.py
@@ -1,15 +1,53 @@
 import time
+import json
 import paho.mqtt.client as mqtt
 from song import AudioHandler
+from node_net import NodeNet, Node
 
+def handle_node_connect(client, userdata, msg):
+    """
+    Ex:
+        {
+            number: 1,
+            name: "classroom"
+        }
+    """
+    print "Handle node connect..."
+
+    data = json.loads(msg.payload)
+    node = node_net.add_node(data["number"], data["name"])
+    print "Added " + str(node)
+
+
+def handle_node_sensor(client, userdata, msg):
+    print("Handle node sensor...")
+
+topics_subscribed = {
+    "node/connect": handle_node_connect,
+    "node/sensor": handle_node_sensor,
+}
 
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
+    print "Connected with result code "+str(rc)
 
-    client.subscribe("song")
+    client.subscribe("song/info")
+    client.subscribe("song/stream")
+
+    client.subscribe("node/connect")
+    client.subscribe("node/sensor")
+    client.subscribe("node/neighbours")
+    
 
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
+    # print("Topic: " + msg.topic)
+    # print(str(msg.payload))
+
+    try:
+        callback = topics_subscribed[msg.topic]
+        callback(client, userdata, msg)
+        
+    except KeyError:
+        pass
 
 
 hostname = "localhost"
@@ -23,16 +61,22 @@ client.on_message = on_message
 client.connect(hostname, port, keep_alive)
 client.loop_start()
 
+node_net = NodeNet()
+
+song = {
+    "name": "jump_8k", 
+    "path": "songs/jump_8k.u8",
+}
+audio = AudioHandler(song["path"])
 
 while True:
-
-    audio = AudioHandler("songs/jump_8k.u8")
+       
+    client.publish("song/info", json.dumps(song))
 
     for chunk in audio.get_chunks():
-        client.publish("song", chunk)
+        client.publish("song/stream", chunk)
 
-        time.sleep(1)
+        time.sleep(2)
 
-    client.loop_stop()
-    break
+    print "Restart song"
 
